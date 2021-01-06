c528bcf4094e85454b4837afa7eb94839b8287ad
Weronika Jaworek <s18630@pjwstk.edu.pl>
2021-01-06 22:30:22 +0100

    Przygotowany kod do cwiczenia6

---
+++ Cwiczenie6/Cwiczenie5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenie5.DTOs.Requests;
+using Cwiczenie5.DTOs.Responses;
+using Cwiczenie5.Models;
+using Cwiczenie5.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie5.Controllers
+{
+  
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private IStudentsDbService _service;
+
+        public EnrollmentsController(IStudentsDbService service)
+        {
+            _service = service;
+        }
+
+
+
+
+
+        [HttpPost]
+        [Route("api/enrollments")]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+
+            try
+            {
+
+                EnrollStudentResponse response = _service.EnrollStudent(request);
+                string ConString = response.getConnectionString();
+                return Created(ConString, response);
+            }
+
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+
+        }
+
+
+
+
+
+
+
+        [HttpPost("api/enrollments/promotions")]
+
+        public IActionResult PromoteStudents(PromoteStudentsRequest request)
+        {
+            
+               PromoteStudentsResponse response= _service.PromoteStudents(request);
+                
+                    string ConString = response.getConnectionString();
+                    return Created(ConString, response);
+
+
+                
+                
+          
+          
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
---
+++ Cwiczenie6/Cwiczenie5/Services/IStudentsDbService.cs
@@ -0,0 +1,17 @@
+﻿using Cwiczenie5.DTOs.Requests;
+using Cwiczenie5.DTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenie5.Services
+{
+    public interface IStudentsDbService
+    {
+
+        EnrollStudentResponse EnrollStudent(EnrollStudentRequest request);
+        PromoteStudentsResponse PromoteStudents(PromoteStudentsRequest request);
+
+    }
+}
